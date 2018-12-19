89e3039de1ee15a912468bb4aae371d46337e7f3
Philip Patton <ppatton35745@gmail.com>
2018-12-18 21:33:46 -0600

    first interactive CL python

--- basics.py
+++ basics.py
@@ -1,1 +1,8 @@
-print("Hello, World")
+first_name = input("WHat is your first name? ")
+
+print(first_name)
+print("These", "will be", "seperated by spaces")
+
+print("Hello, Phil")
+print("Phil is learning Python")
+print(first_name, "is using the variable")
